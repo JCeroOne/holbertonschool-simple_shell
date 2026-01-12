857fdfa6bce370777f98b9f9124f5ef97d01131e
JCeroOne <jceroone@gmail.com>
2026-01-12 18:08:40 +0000

    Primitive advances

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include "simpleshellfn.h"
 
-void main (int argc, char *argv[], char **envp)
+void input(char **cmd, size_t *size)
 {
-	return (0);
+	int read_chars;
+
+	read_chars = getline(cmd, size, stdin);
+
+	if (read_chars == -1)
+	{
+		if(isatty(STDIN_FILENO) != 0)
+			printf("\n");
+		
+		exit(EXIT_SUCCESS);
+	}
+
+	if((*cmd)[read_chars - 1] == '\n')
+		(*cmd)[read_chars - 1] = '\0';
+}
+
+int main(int argc, char *argv[], char **envp)
+{
+	char *cmd = NULL;
+	size_t size = 0;
+
+	printf("%d %s %s", argc, argv[0], *envp);
+
+	while(1)
+	{
+		if(isatty(STDIN_FILENO))
+			printf("($) ");
+
+		input(&cmd, &size);
+
+		if(strcmp(cmd, "exit") == 0)
+			exit(EXIT_SUCCESS);
+
+		else
+			printf("Nein");
+	}
 }
--- simpleshellfn.h
+++ simpleshellfn.h
@@ -1,7 +1,7 @@
 #ifndef _SIMPLESHELLFN_H_
 #define _SIMPLESHELLFN_H_
 
-void input(char **command, size_t size);
+void input(char **command, size_t *size);
 void parse(char command[], char **envp);
 int exec(char *command[], char **envp);
 char **get_path(char **cmd, char **envp);
