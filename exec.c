42f79dbaf8963b1754aea4d0339a12ab6c84d983
JCeroOne <jceroone@gmail.com>
2026-01-13 19:38:40 +0000

    Advances

---
+++ exec.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "simpleshellfn.h"
+
+char **parse(char *cmd)
+{
+	char *copy = strdup(cmd);
+	char *tok = NULL;
+	char **args = NULL;
+	int i = 0;
+	int argc = 0;
+
+	if(!copy)
+		return (NULL);
+
+	tok = strtok(copy, " ");
+
+	while(tok)
+	{
+		argc++;
+		tok = strtok(NULL, " ");
+	}
+
+	free(copy);
+	args = malloc((argc + 1) * sizeof(char *));
+
+	if(!args)
+		return (NULL);
+	
+	copy = strdup(cmd);
+
+	if(!copy)
+	{
+		free_args(args);
+		return (NULL);
+	}
+
+	tok = strtok(copy, " ");
+
+	for (i = 0; tok; i++)
+	{
+		args[i] = strdup(tok);
+
+		if(!args[i])
+		{
+			free_args(args);
+			free(copy);
+			return (NULL);
+		}
+
+		tok = strtok(NULL, " ");
+	}
+
+	args[i] = NULL;
+	free(copy);
+
+	return (args);
+}
+
+int exec(char *cmd, char **envp)
+{
+	char **args = parse(cmd);
+}
--- main.c
+++ main.c
@@ -6,13 +6,18 @@
 #include <fcntl.h>
 #include "simpleshellfn.h"
 
+/**
+ * input - Waits for and reads input from the user.
+ * @cmd: A pointer to the cmd variable (char *)
+ * @size: A pointer to the size variable (size_t)
+*/
 void input(char **cmd, size_t *size)
 {
 	int read_chars;
 
 	read_chars = getline(cmd, size, stdin);
 
-	if (read_chars == -1)
+	if (read_chars == EOF)
 	{
 		if(isatty(STDIN_FILENO) != 0)
 			printf("\n");
@@ -24,17 +29,26 @@ void input(char **cmd, size_t *size)
 		(*cmd)[read_chars - 1] = '\0';
 }
 
+/*
+ * main - Entry point for the shell
+ * @argc: The number of arguments passed
+ * @argv: The values of the arguments
+ * @envp: The environment data
+ *
+ * Return: 0 if succesful, an error code otherwise.
+*/
 int main(int argc, char *argv[], char **envp)
 {
 	char *cmd = NULL;
 	size_t size = 0;
 
-	printf("%d %s %s", argc, argv[0], *envp);
+	printf("[DEBUG] %d %s %s\n", argc, argv[0], *envp);
+	printf("\nWelcome to JC's Simple Shell!\nTo close the shell, type 'exit'\n");
 
 	while(1)
 	{
 		if(isatty(STDIN_FILENO))
-			printf("($) ");
+			printf("\n($) ");
 
 		input(&cmd, &size);
 
@@ -42,6 +56,6 @@ int main(int argc, char *argv[], char **envp)
 			exit(EXIT_SUCCESS);
 
 		else
-			printf("Nein");
+			exec(cmd, envp);
 	}
 }
